6f8402cb622b118dd8238f5c94b2a3b80249f453
Alex <tinktink0905@gmail.com>
2026-04-11 16:33:19 -0700

    card scripts working
    Only the script is working, we don't have them actually implemented yet

---
+++ KKC-GAMEDEV-CLUB-2D/Assets/card scripts/3. DeckManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CardClasses;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<Card> allCards = new List<Card>();
+    //put cards in Resources Folder not CardData
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        //Load all cards from the resources folder
+        Card[] cards = Resources.LoadAll<Card>("Cards");
+
+        //Add the loaded cards to the allcards list
+        allCards.AddRange(cards);
+
+        HandManager hand = FindFirstObjectByType<HandManager>();
+        for (int i = 0; i < 6; i++){
+            DrawCard(hand);
+        }
+    }
+
+    public void DrawCard(HandManager handManager)
+    {
+        if (allCards.Count == 0)
+            return;
+
+            Card nextCard = allCards[currentIndex];
+        handManager.AddCardToHand(nextCard);
+        currentIndex = (currentIndex + 1) % allCards.Count;
+
+        HandManager hand = FindFirstObjectByType<HandManager>();
+        for (int i = 0; i < 6; i++)
+        {
+            DrawCard(hand);
+        }
+    }
+}
