ff29809765fffc1319b3142baa13a0ea2e6f296d
Wolfyl <lasonenathan@gmail.com>
2026-07-13 21:21:40 -0500

    little work done
    started messing with adding turn based

---
+++ KKC-GAMEDEV-CLUB-2D/Assets/Scripts/Systems/GameAction.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public abstract class GameAction
+{
+    public List<GameAction> PreReactions { get; private set; } = new();
+    public List<GameAction> PerformReactions { get; private set; } = new();
+    public List<GameAction> PostReactions { get; private set; } = new();
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
